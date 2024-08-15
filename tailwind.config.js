4503a2beb53bc29b68647acacd029d35a48ed99e
Ratiarisandy Dinoh <dinohratiarisandy@gmail.com>
2024-08-15 21:42:25 +0100

    config files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["dark"],
+    base: true,
+    styled: true,
+    utils: true,
+    themeRoot: ":root"
+  },
+}
