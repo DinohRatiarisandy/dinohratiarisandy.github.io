4241c8c80a6cb30d9edea3f811eb5bd65449c68e
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-08-26 17:44:23 +0100

    add tic-tac-toe to fun projects and reorganize

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,29 +8,35 @@ import tetrisImg from "../assets/icon/tetris-img.png"
 import sokobanImg from "../assets/icon/sokoban-img.png"
 import geneticAlgoImg from "../assets/icon/genetic-algo-img.png"
 import connectFourImg from "../assets/icon/connect-4-img.png"
+import tictactoeImg from "../assets/icon/tic-tac-toe-img.png"
 
 const PROJECTS = [
    {
-      image: geneticAlgoImg,
-      title: "Genetic-Algorithm--Knapsack-problem",
-      description: "Knapasack problem solved by the genetic algorithm.",
+      image: connectFourImg,
+      title: "Connect-4 with AI",
+      description: "Connect-4 game with unbeatable bot (AI).",
       urlGithub:
-         "https://github.com/DinohRatiarisandy/Genetic-Algorithm--Knapsack-problem",
+         "https://DinohRatiarisandy.github.io/connect-four-game-with-AI",
    },
    {
       image: img2048,
       title: "2048-game with AI",
       description:
          "2048-game with expectimax algorithm for the autoplay (botSolver/AI).",
-      urlGithub:
-         "https://github.com/DinohRatiarisandy/2048-game-with-expectimax",
+      urlGithub: "https://dinoh-sandys.itch.io/2048-with-expectimax",
+   },
+   {
+      image: tictactoeImg,
+      title: "Tic-Tac-Toe unbeatable",
+      description: "The old tic-tac-toe game with unbeatable AI.",
+      urlGithub: "https://DinohRatiarisandy.github.io/TicTacToeJS/",
    },
    {
       image: eightPuzzleImg,
       title: "8-puzzle game",
       description:
          "8-puzzle game made with pygame using A* to solve the puzzle.",
-      urlGithub: "https://github.com/DinohRatiarisandy/8-puzzle-game",
+      urlGithub: "https://dinoh-sandys.itch.io/8-puzzle-game-with-ai",
    },
    {
       image: apiImg,
@@ -39,30 +45,30 @@ const PROJECTS = [
       urlGithub: "https://github.com/DinohRatiarisandy/Sudoku-API",
    },
    {
+      image: tetrisImg,
+      title: "Tetris game",
+      description: "Tetris game clone built with PyGame.",
+      urlGithub: "https://github.com/DinohRatiarisandy/Tetris-game/releases",
+   },
+   {
+      image: sokobanImg,
+      title: "Sokoban-Remake",
+      description:
+         "Remake the old game Sokoban with godot engine. Woooww ! Nice look !",
+      urlGithub: "https://dinoh-sandys.itch.io/sokoban-remake",
+   },
+   {
       image: skyscraperImg,
       title: "Skyscrapers-game solver",
       description: "Solver (resolution algorithm) for the skyscrapers game.",
       urlGithub: "https://github.com/DinohRatiarisandy/Skyscrapers_Game_AI",
    },
    {
-      image: tetrisImg,
-      title: "Tetris game",
-      description: "Tetris game clone built with PyGame.",
-      urlGithub: "https://github.com/DinohRatiarisandy/Tetris-game",
-   },
-   {
-      image: sokobanImg,
-      title: "Sokoban-Remake",
-      description:
-         "Remake the old game Sokoban with godot engine. Woooww ! Nice face !",
-      urlGithub: "https://github.com/DinohRatiarisandy/Sokoban-Remake",
-   },
-   {
-      image: connectFourImg,
-      title: "Connect-4 with AI",
-      description: "Connect-4 game with unbeatable bot (AI).",
+      image: geneticAlgoImg,
+      title: "Genetic-Algorithm--Knapsack-problem",
+      description: "Knapasack problem solved by the genetic algorithm.",
       urlGithub:
-         "https://github.com/DinohRatiarisandy/connect-four-game-with-AI",
+         "https://github.com/DinohRatiarisandy/Genetic-Algorithm--Knapsack-problem/releases",
    },
 ]
 
