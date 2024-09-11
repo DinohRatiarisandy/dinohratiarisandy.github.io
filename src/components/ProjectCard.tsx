4cee6b1673ebe4ca377e987fda823a049a75bad2
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-11 23:59:30 +0100

    fix: fix props variables name

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,24 +1,28 @@
+import Badge from "./ui/Badge"
+
 type ProjectProps = {
-   image: string
+   thumbnail: string
    title: string
    description: string
-   urlGithub: string
+   url: string
+   isNew?: boolean
 }
 
-function ProjectCard(props: ProjectProps) {
+function ProjectCard({ isNew = false, ...props }: ProjectProps) {
    return (
       <a
          target="_blank"
-         href={props.urlGithub}
-         className="flex items-center gap-2 p-2 transition-colors hover:bg-slate-500/10"
+         href={props.url}
+         className="relative flex items-center gap-2 p-2 transition-colors hover:bg-slate-500/10"
       >
-         <img src={props.image} alt="" className="h-12 rounded" />
+         <img src={props.thumbnail} alt="" className="h-12 rounded" />
          <div className="flex flex-col gap-1">
             <h4 className="font-semibold text-slate-300">{props.title}</h4>
             <article className="text-sm text-slate-500">
                {props.description}
             </article>
          </div>
+         {isNew && <Badge text="New" className="absolute right-5 top-3" />}
       </a>
    )
 }
