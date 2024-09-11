8e900eda2ff0ea186fe07292a28e68162e8466e8
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-11 23:51:24 +0100

    feat: badge to hightlight new projects

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef } from "react"
+
+type BadgeProps = ComponentPropsWithoutRef<"span"> & {
+   text: string
+}
+
+function Badge(props: BadgeProps) {
+   return (
+      <span
+         className={`${props.className} w-9 rounded-full bg-green-500/50 px-1 text-center text-xs text-white`}
+      >
+         {props.text}
+      </span>
+   )
+}
+
+export default Badge
