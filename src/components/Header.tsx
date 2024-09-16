2730c73ddc83e6f471ddcf0cf836502f9c7c985f
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-16 22:32:30 +0100

    feat: add toggle theme button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import SocialNetwork from "./SocialNetwork"
+import ToggleTheme from "./ui/ToggleTheme"
 
 function Header() {
    return (
       <div className="sticky top-0 z-10 flex items-center justify-between border-b border-slate-400/20 px-2 backdrop-blur">
          <p className="my-4 flex-[2] font-bold text-slate-400">dinoh__sandys</p>
-         <SocialNetwork />
+         <div className="flex gap-8">
+            <ToggleTheme />
+            <SocialNetwork />
+         </div>
       </div>
    )
 }
