0bc90feee48106fb7647779f0354a399b595c4e5
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2024-09-26 20:57:03 -0700

    feat: add peoplefor ai

--- src/constants/works.ts
+++ src/constants/works.ts
@@ -1,13 +1,14 @@
 import carImg from "../assets/thumbnails/car-logo.png"
 import metfpImg from "../assets/thumbnails/metfp-logo.png"
+import peopleforAiImg from "../assets/thumbnails/peopleforai-logo.jpg"
 
 export const WORKS = [
    {
-      entrepriseImage: metfpImg,
-      entrepriseName: "M.E.T.F.P",
-      entrepriseWebsite: "https://www.metfp.gov.mg",
-      myPost: "Developer ReactJS",
-      year: "2021",
+      entrepriseImage: peopleforAiImg,
+      entrepriseName: "People for AI",
+      entrepriseWebsite: "https://www.peopleforai.com",
+      myPost: "Developer Python (Data Expert)",
+      year: "2024",
    },
    {
       entrepriseImage: carImg,
@@ -15,5 +16,12 @@ export const WORKS = [
       entrepriseWebsite: "http://cartaxaudit.com",
       myPost: "Developer Odoo",
       year: "2024",
+   },
+   {
+      entrepriseImage: metfpImg,
+      entrepriseName: "M.E.T.F.P",
+      entrepriseWebsite: "https://www.metfp.gov.mg",
+      myPost: "Developer ReactJS",
+      year: "2021",
    },
 ]
